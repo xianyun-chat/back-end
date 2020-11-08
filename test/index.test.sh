d42e89d7c79873f7aafb42c331aa3ecd354ec922
三文 <zhiwen.szw@dtwave-inc.com>
2020-11-08 20:31:46 +0800

    支持生成测试报告

--- test/index.test.sh
+++ test/index.test.sh
@@ -2,12 +2,12 @@
 cd test/sql
 
 # 自定义测试执行的顺序
-mocha logon.test.js
-mocha login.test.js
-mocha modifyPassword.test.js
-mocha modifyUserName.test.js
-mocha createChatRoom.test.js
-mocha getChatRoomByClass.test.js
-mocha getChatRoomByID.test.js
-mocha createMessage.test.js
-mocha getChatHistory.test.js
+mocha --reporter mochawesome logon.test.js login.test.js
+mocha --reporter mochawesome login.test.js
+mocha --reporter mochawesome modifyPassword.test.js
+mocha --reporter mochawesome modifyUserName.test.js
+mocha --reporter mochawesome createChatRoom.test.js
+mocha --reporter mochawesome getChatRoomByClass.test.js
+mocha --reporter mochawesome getChatRoomByID.test.js
+mocha --reporter mochawesome createMessage.test.js
+mocha --reporter mochawesome getChatHistory.test.js
